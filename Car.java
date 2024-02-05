01fe0b4d4275039c4afeca1e2eefd0bf4b88931d
Shuji Sugimoto <shuji.sugimoto@rakus-partners.co.jp>
2024-02-05 11:51:10 +0900

    TEST2

--- Car.java
+++ Car.java
@@ -1,7 +1,7 @@
 public class Car {
     public static void main(String[] args) {
         System.out.println("aaa");
-        System.out.println("ddd");
+        System.out.println("eee");
         System.out.println("ccc");
     }
 }
